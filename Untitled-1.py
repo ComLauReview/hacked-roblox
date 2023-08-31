e03035e50658bc721b37ee8d9af14a63e9b1209c
ShungChingg <120564607+ComLauReview@users.noreply.github.com>
2023-08-31 18:08:42 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,39 @@
+print('|||||       |||||')
+print('|||||       |||||')
+print('||||| ||||| |||||')
+print('|||||       |||||')
+print('|||||       |||||')
+print(' Welcome to H , Hacked Account ROBLOX ')
+print('-----------------------------------')
+print('IP Web Roblox: ')
+ip = input()
+print('Username Account: ')
+user = input()
+print('Key: ')
+key = input()
+
+if key == '324678793269489234298364892389489234898293468972368946':
+    print('____________________________________________')
+    print('Set Up (YES) or (NO)')
+    st = input()
+    if st == 'YES':
+            print(user)
+            print(key)
+            print('longhacker2005@#')
+            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            print('Do U Want To Change Pass? YES or NO ')
+            Change = input()
+   
+
+            if Change == 'YES':
+                print('INPUT PASS:')
+                passc = input()
+                print('Successfully change pass ', user )
+            if Change == 'NO':
+                print('END')
+    if st == 'NO':
+            exit()
+else:
+    print('IP UPDATE 1.128.432.231')
+
+      
